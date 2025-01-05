082c2c4710f2908ff5b5e4442e5414694f47058d
manik1312006 <manikmehtaji@gmail.com>
2025-01-05 19:30:02 +0530

    UI/script.js

--- UI/script.js
+++ UI/script.js
@@ -181,8 +181,8 @@ document.addEventListener('DOMContentLoaded', function() {
         const parentDiv = document.createElement('div');
         parentDiv.className = 'parent';
         parentDiv.innerHTML = `
-            <div class="ch" onclick="window.open(${"vlc://https://"+movie.link})>
-                <a href="">
+            <div class="ch">
+                <a href="${"https://"+movie.link}">
                     <img src="${movie.imageUrl}" class="img" alt="${movie.title}">
                     <span>${movie.title}</span>
                 </a>
