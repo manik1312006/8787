e121e480d8390bc67d6ad4c0f26faf31b9a55257
manik1312006 <manikmehtaji@gmail.com>
2025-04-05 17:42:25 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+f = input("Enter File Name(Without Extention): ")
+with open(f"{f}.m3u8", "w") as file:
+    file.write(f"#EXTM3U{"\n"}")
+while True:
+    Title = input("Enter Title(Write Done if done.): ")
+    if Title.lower() != "done":
+        Link = input("Enter Link: ")
+        with open(f"{f}.m3u8", "a") as file:
+            file.write(f"#EXTINF:-1, {Title}{"\n"}")
+            file.write(f"{Link}{"\n"}")
+    else:
+        break
